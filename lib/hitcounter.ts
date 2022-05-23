3d9a223f0f4b9519aa616b7dda95b55daa0ad9ca
mels-tovmasyan <tovmasyan.mels@gmail.com>
2022-05-23 08:17:07 +0400

    updates

---
+++ lib/hitcounter.ts
@@ -0,0 +1,36 @@
+//sorry for "hayatar" comments, for now I am more comfortable with them ))
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { Construct } from 'constructs';
+
+export interface HitCounterProps {
+    downstream: lambda.IFunction;
+}
+
+export class HitCounter extends Construct {
+
+    public readonly handler: lambda.Function;
+
+    public readonly table: dynamodb.Table;
+
+
+    constructor(scope: Construct, id: string, props: HitCounterProps) {
+        super(scope, id);
+
+        this.table = new dynamodb.Table(this, 'Hits', { partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING }});
+
+        this.handler = new lambda.Function(this, 'HitCounterHandler', {
+            runtime: lambda.Runtime.NODEJS_14_X,
+            handler: 'hitcounter.handler',
+            code: lambda.Code.fromAsset('lambda'),
+            environment : {
+                DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+                HITS_TABLE_NAME: this.table.tableName
+            }
+        });
+
+        this.table.grantReadWriteData(this.handler); //estex lambdais permission em talis, vor karana dynamo-ums gri
+        props.downstream.grantInvoke(this.handler); // estex lyambdas invoke anelu permission em tali alam ashxarhin
+    }
+}
